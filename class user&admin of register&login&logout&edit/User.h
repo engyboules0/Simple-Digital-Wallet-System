dac739819c88a2bfeb46549121ed7745ec7ee079
engyboules0 <162360340+engyboules0@users.noreply.github.com>
2024-04-20 22:45:43 +0200

    Create User.h
    This code of class user(file.h) 
    under review

---
+++ class user&admin of register&login&logout&edit/User.h
@@ -0,0 +1,35 @@
+#include <string>
+#include <vector>
+using namespace std;
+
+class User {
+private:
+    string username;
+    string password;
+    double balance;
+    
+
+public:
+    User(string, string );
+    static vector<User> users;
+    void registerUser();
+    void loginUser();
+
+    string getUsername() const;
+
+    void setPassword(const string&);
+    bool authenticate(string);
+    void editProfile();
+    void logout();
+    void saveToFile() const;
+    void loadFromFile();
+
+
+private:
+    bool hasUpperCase(string);
+    bool hasLowerCase(string);
+    bool hasDigit(string );
+    void changePassword(string);
+    bool isStrongPassword(string );
+};
+
